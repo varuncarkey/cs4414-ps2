aadcdcb122ada81050a60140d8506c3f74ccf044
varuncarkey <vk2em@virginia.edu>
2014-02-09 23:02:54 -0500

    MULTIPLE PIPING

--- gash.rs
+++ gash.rs
@@ -22,6 +22,7 @@ use std::io::signal::{Listener, Interrupt};
 use std::io::pipe::PipeStream;
 use std::str;
 
+
 struct Shell {
     cmd_prompt: ~str,
     cwd: Path,
@@ -184,7 +185,9 @@ impl Shell {
             {
                 if argv[i]==~"<"||argv[i]==~">" || argv[i]==~"|"
                 {
-                    if argv[i]==~">"
+                
+                process_done = true;
+                    /*if argv[i]==~">"
                     {
                         let mut argv2=argv.clone().to_owned();
                         argv2.remove(0);
@@ -239,23 +242,220 @@ impl Shell {
                         let mut newprocess2=run::Process::new(program.to_owned(), argv.to_owned(), options2).unwrap();
                         newprocess.input().write(output);
 
-                    }
+                    }*/
+                    
+                    
+                    
                    
                 }
                 
             }
+            
+            if process_done{
 
-            if !process_done
+                self.multiCulturalloop(Some(program.to_owned()), None, argv.to_owned());
+            }
+
+            else if !process_done
                 {run::process_status(program, argv);}
 
         } else {
             println!("{:s}: command not found", program);
         }
     }
-    fn multiloop()
-    {
     
+    fn multiCulturalloop(&mut self, prgm: Option<~str>, output: Option<~str>, mut argv: ~[~str]){
+        match prgm{
+            Some(prgm) => {
+            let mut vec: ~[~str]=~[];
+            for i in range(0, argv.len()){
+                        
+                if argv[i]==~"|"
+                    {
+                        let mut roundtwo=false;
+                        match output.clone()
+                        {
+                            Some(out) => {roundtwo=true;}
+                            None => {roundtwo=false;}
+                        }
+                    if !roundtwo{
+                     let mut f;
+                       f= match native::io::file::open(&".outfile.md".to_c_str(),
+                                             Open,Write) {
+                                                            Ok(f)  => f,
+                                                            Err(e) => fail!("{}",e.to_str())
+                                                        };
+                        let fd=f.fd();
+                        let mut options =run::ProcessOptions::new();
+                            options.in_fd =Some(0);
+                            options.out_fd=Some(fd);
+                            options.err_fd=Some(2);
+                        
+                        let mut newProcess= run::Process::new(prgm.to_owned(), vec,options).unwrap();
+                        let outputNew = newProcess.finish_with_output().output;
+                        
+                        let mut newVec: ~[~str] = ~[]; 
+                        let prg=argv[i+1].clone();
+                        for j in range(i+2, argv.len()){
+                            newVec.push(argv[j].clone());
+                        }
+                        return self.multiCulturalloop(Some(prg), Some(~""), newVec);
+                        
+                        
+                        //let mut d = File::create(&Path::new(".outfile2.md"));
+                        //d.write(outputNew);
+                        
+                        //run::process_status("rm", [~".outfile.md"]);
+                        
+                        //let fname = ".outfile.md";
+                        //let mut file = File::create(&Path::new(".outfile.md"));
+                        //file.write(outputNew);
+                        
+                        //let outputNew=newProcess.output().read_to_end();
+                        //print!("FIRST {}",output.to_str());
+                        
+                      }
+                      else
+                      {
+                        
+                        let f= match native::io::file::open(&".outfile.md".to_c_str(),
+                                             Open,Read) {
+                                                            Ok(f)  => f,
+                                                            Err(e) => fail!("{}",e.to_str())
+                                                        };
+                        let fd1=f.fd();
+                        
+                        let r= match native::io::file::open(&".outfile2.md".to_c_str(),
+                                             Open,Write) {
+                                                            Ok(f)  => f,
+                                                            Err(e) => fail!("{}",e.to_str())
+                                                        };
+                        let fd2=r.fd();
+                        
+                        let mut options =run::ProcessOptions::new();
+                        options.in_fd =Some(fd1);
+                        options.out_fd=Some(fd2);
+                        options.err_fd=Some(2);
+                        
+                        let mut oldProcess= run::Process::new(prgm.to_owned(),vec,options).unwrap();
+                        let outputNew = oldProcess.finish_with_output().output;
+                        println!("OUTPUt : {}",outputNew.to_str());
+                        
+                        run::process_status("rm", [~".outfile.md"]);
+                        let path=Path::new(".outfile.md");
+                        let path2=Path::new(".outfile2.md");
+                        let mut file=File::open_mode(&path2, Open, Read);
+                        let mut d=File::open_mode(&path, Open, Write);
+                        d.write(file.read_to_end());
+                        run::process_status("rm", [~".outfile2.md"]);
+                        //let mut d = File::create(&Path::new(".outfile.md"));
+                        let mut newVec: ~[~str] = ~[]; 
+                        let prg=argv[i+1].clone();
+                        for j in range(i+2, argv.len()){
+                            newVec.push(argv[j].clone());
+                        }
+                        return self.multiCulturalloop(Some(prg), Some(~""), newVec);
+                        
+                      
+                      }
+
+                    } else{
+                    
+                        vec.push(argv[i].clone());
+                        
+                    }
+            }
+            println!("prgm: {}",prgm);
+            let filename=".outfile.md";
+                let f = match native::io::file::open(&filename.to_c_str(),
+                                         Open, Read) {
+                                                        Ok(f)  => f,
+                                                        Err(e) => fail!("{}",e.to_str())
+                                                    };
+
+                let fd=f.fd();
+                let mut options =run::ProcessOptions::new();
+                            options.in_fd =Some(fd);
+                            options.out_fd=Some(1);
+                            options.err_fd=Some(2);
+                let mut newProcess = run::Process::new(prgm.to_owned(), argv,options).unwrap();
+                
+                let outputNew=(newProcess.finish_with_output().output);//std::str::from_utf8(newProcess.output().read_to_end()).to_owned();
+                return;
+            
+            }
+            None => {
+            
+               /* let mut vec: ~[~str]=~[];
+                let prg= argv.remove(0);
+                for k in range(0, argv.len()){
+          
+                    match argv[k]{
+                    
+                        ~"|" =>{
+                            let filename=".outfile.md";
+                            let file2=".outfile2.md";
+                            let f = match native::io::file::open(&filename.to_c_str(),
+                                             Open, Read) {
+                                                            Ok(f)  => f,
+                                                            Err(e) => fail!("{}",e.to_str())
+                                                        };
+                            let fd=f.fd();
+                        
+                            let r = match native::io::file::open(&file2.to_c_str(),
+                                             Open, Write) {
+                                                            Ok(f)  => f,
+                                                            Err(e) => fail!("{}",e.to_str())
+                                                        };
+    
+                            let fd1=r.fd();
+                            let mut options =run::ProcessOptions::new();
+                                options.in_fd =Some(fd);
+                                options.out_fd=Some(fd1);
+                                options.err_fd=Some(2);
+                            let mut newProcess = run::Process::new(prg.to_owned(), vec,options).unwrap();
+                            let outputNew=newProcess.finish_with_output().output;
+                            //run::process_status("rm", [~".outfile.md"]);
+                            //let mut d = File::create(&Path::new(".outfile.md"));
+                            //d.write(outputNew);
+                            println!("OUT PUT: {}", outputNew.to_str());
+                            
+                            let mut newVec: ~[~str] = ~[];
+                            for j in range(k+1, argv.len()){
+                                newVec.push(argv[j].clone());
+                            }
+                            return self.multiCulturalloop(None, Some(outputNew), newVec);
+                           }
+                          _ => {
+                            vec.push(argv[k].clone());
+                            }
+                    }
+                }*/
+               /* let filename=".outfile.md";
+                let f = match native::io::file::open(&filename.to_c_str(),
+                                         Open, Read) {
+                                                        Ok(f)  => f,
+                                                        Err(e) => fail!("{}",e.to_str())
+                                                    };
+
+                let fd=f.fd();
+                let mut options =run::ProcessOptions::new();
+                            options.in_fd =Some(fd);
+                            options.out_fd=Some(1);
+                            options.err_fd=Some(2);
+                let mut newProcess = run::Process::new(prg.to_owned(), vec,options).unwrap();
+                
+                let outputNew=(newProcess.finish_with_output().output);//std::str::from_utf8(newProcess.output().read_to_end()).to_owned();
+                return;*/
+            
+            
+            }
+            
+        }
+
     }
+    
+    
     fn cmd_exists(&mut self, cmd_path: &str) -> bool {
         let ret = run::process_output("which", [cmd_path.to_owned()]);
         return ret.expect("exit code error.").status.success();
---
+++ outfile2.md
@@ -0,0 +1,44 @@
+lo0: flags=8049<UP,LOOPBACK,RUNNING,MULTICAST> mtu 16384
+	options=3<RXCSUM,TXCSUM>
+	inet6 ::1 prefixlen 128 
+	inet 127.0.0.1 netmask 0xff000000 
+	inet6 fe80::1%lo0 prefixlen 64 scopeid 0x1 
+	nd6 options=1<PERFORMNUD>
+gif0: flags=8010<POINTOPOINT,MULTICAST> mtu 1280
+stf0: flags=0<> mtu 1280
+en0: flags=8863<UP,BROADCAST,SMART,RUNNING,SIMPLEX,MULTICAST> mtu 1500
+	ether 60:03:08:8f:89:8a 
+	inet6 fe80::6203:8ff:fe8f:898a%en0 prefixlen 64 scopeid 0x4 
+	inet 172.25.208.168 netmask 0xffffff00 broadcast 172.25.208.255
+	nd6 options=1<PERFORMNUD>
+	media: autoselect
+	status: active
+en1: flags=8963<UP,BROADCAST,SMART,RUNNING,PROMISC,SIMPLEX,MULTICAST> mtu 1500
+	options=60<TSO4,TSO6>
+	ether 72:00:01:19:1d:d0 
+	media: autoselect <full-duplex>
+	status: inactive
+en2: flags=8963<UP,BROADCAST,SMART,RUNNING,PROMISC,SIMPLEX,MULTICAST> mtu 1500
+	options=60<TSO4,TSO6>
+	ether 72:00:01:19:1d:d1 
+	media: autoselect <full-duplex>
+	status: inactive
+bridge0: flags=8863<UP,BROADCAST,SMART,RUNNING,SIMPLEX,MULTICAST> mtu 1500
+	options=63<RXCSUM,TXCSUM,TSO4,TSO6>
+	ether 62:03:08:f8:12:00 
+	Configuration:
+		id 0:0:0:0:0:0 priority 0 hellotime 0 fwddelay 0
+		maxage 0 holdcnt 0 proto stp maxaddr 100 timeout 1200
+		root id 0:0:0:0:0:0 priority 0 ifcost 0 port 0
+		ipfilter disabled flags 0x2
+	member: en1 flags=3<LEARNING,DISCOVER>
+	        ifmaxaddr 0 port 5 priority 0 path cost 0
+	member: en2 flags=3<LEARNING,DISCOVER>
+	        ifmaxaddr 0 port 6 priority 0 path cost 0
+	nd6 options=1<PERFORMNUD>
+	media: <unknown type>
+	status: inactive
+p2p0: flags=8843<UP,BROADCAST,RUNNING,SIMPLEX,MULTICAST> mtu 2304
+	ether 02:03:08:8f:89:8a 
+	media: autoselect
+	status: inactive
